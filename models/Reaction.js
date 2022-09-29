501982488b53821329e46d09bdf043392d671c7d
Matt <mwhlavaty@gmail.com>
2022-09-29 10:08:54 -0700

    initial commit

---
+++ models/Reaction.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose")
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Must match an email address",
+    ],
+  },
+});
